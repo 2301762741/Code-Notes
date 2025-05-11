e227a1f23cf19d997f5238d7992fa86af1bbf921
while-XH <2301762741.net>
2025-05-11 15:07:07 +0800

    嵌入式合并目录

--- RTOS/STM32F401/Fly2/Hardware/ICa.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ICA_H
-#define __ICA_H
-
-void ICa_Init(void);
-uint32_t ICa_GetFreq(void);
-uint32_t ICa_GetDuty(void);
-
-#endif
--- RTOS/STM32F401/Fly2/System/Delay.h
+++
@@ -1,8 +0,0 @@
-#include "stm32f4xx.h"                  // Device header
-#include "ucos_ii.h"
-#include "core_cm4.h"
-#include "os_cpu.h"
-
-void Delay_Init(u8 SYSCLK);
-void Delay_us(u32 nus);
-void Delay_ms(u16 nms);
